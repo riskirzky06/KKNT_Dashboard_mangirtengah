39a473665893b030a89e782d65f38501040f7376
riskirzky06 <denisariski2312@gmail.com>
2024-02-19 18:52:08 +0700

    undo

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -23,31 +23,5 @@ class Home extends CI_Controller
         maka CRUD Admin pembalajaran sebelumnya mengikuti script seperti pada
         control Home */
         $this->template->load('template/template', $this->view . 'read', $data);
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto"> <!-- Tambahkan kelas ml-auto untuk membuat item menu rata kanan -->
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      
-
     }
 }
