a85c74870fcd95c40a824387a1e59c87c5e6f434
riskirzky06 <denisariski2312@gmail.com>
2024-02-19 18:54:23 +0700

    undo

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,7 +13,7 @@ class Home extends CI_Controller
     public function index()
     {
         $data = array(
-
+            'judul' => "Welcome To Website Mangir Tengah"
         );
         /*
         $this->template memanggil libraries template,
